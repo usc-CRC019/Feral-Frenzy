b0a93c7c481140f1b7d1b85be6b40f733bed3159
NoLoveRS <casey17798@live.com.au>
2024-04-18 20:22:55 +1000

    Removed camera dampening and added the Player component which currently only locks the cursor while playing and holds some player stats.

---
+++ Assets/Components/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int PlayerLevel;
+    private int Reputation;
+    private int MoveSpeed;
+    private int JumpHeight;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
