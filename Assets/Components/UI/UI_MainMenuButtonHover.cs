2ec1bde1dfc5f1c65b9163ca674ffaed77d6ba49
CRC019 <crc019@student.usc.edu.au>
2024-10-25 19:45:42 +1000

    - Added Morgans default state main menu. - Changed main menu button behaviour to only light up when hovered.

--- Assets/Components/UI/UI_MainMenuButtonHover.cs
+++ Assets/Components/UI/UI_MainMenuButtonHover.cs
@@ -7,7 +7,6 @@ public class UI_MainMenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPoin
 {
     public string buttonName;
     public MainMenu menu;
-    private Sprite lastHovered;
 
     //Detect if the Cursor starts to pass over the GameObject
     public void OnPointerEnter(PointerEventData pointerEventData)
@@ -15,26 +14,15 @@ public class UI_MainMenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPoin
         switch (buttonName)
         {
             default:
-                if (lastHovered != null)
-                {
-                    menu.background.sprite = lastHovered;
-                }
-                else
-                {
-                    menu.background.sprite = menu.backgrounds[0];
-                }
                 break;
             case "Start":
-                menu.background.sprite = menu.backgrounds[0];
-                lastHovered = menu.backgrounds[0];
+                menu.background.sprite = menu.backgrounds[1];
                 break;
             case "Options":
-                menu.background.sprite = menu.backgrounds[1];
-                lastHovered = menu.backgrounds[1];
+                menu.background.sprite = menu.backgrounds[2];
                 break;
             case "Exit":
-                menu.background.sprite = menu.backgrounds[2];
-                lastHovered = menu.backgrounds[2];
+                menu.background.sprite = menu.backgrounds[3];
                 break;
         }
 
@@ -50,6 +38,7 @@ public class UI_MainMenuButtonHover : MonoBehaviour, IPointerEnterHandler, IPoin
     //Detect when Cursor leaves the GameObject
     public void OnPointerExit(PointerEventData pointerEventData)
     {
+        menu.background.sprite = menu.backgrounds[0];
         //Output the following message with the GameObject's name
         //Debug.Log("Cursor Exiting " + name + " GameObject");
     }
